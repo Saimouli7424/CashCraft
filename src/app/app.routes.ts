d0721014b72b11522145e5d583fc4548a62e8e77
sai mouli <saimoulispeed@gmail.com>
2024-03-31 18:02:38 +0530

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from '@angular/router';
+export const routes: Routes = [
+    { path: '', redirectTo: '/budget-planner/login', pathMatch: 'full' },
+    {path:'budget-planner',loadChildren:()=> import('./budget-planner/budget-planner.module').then(m=> m.BudgetPlannerModule)}
+];
